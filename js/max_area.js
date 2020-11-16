31ee1512f4aeb077443aa52b0c2f96992d4869cf
Basant Sharma <basant.sharma@ebutor.com>
2020-11-16 16:23:04 +0530

    Find the first & last element of the sorted array

---
+++ js/max_area.js
@@ -0,0 +1,23 @@
+var maxArea = (height) => {
+
+    l = 0
+    r = height.length -1;
+    ma = 0;
+    while(l > r){
+        ma = (m);
+
+
+    }
+
+
+
+
+    return ma;
+
+
+}
+
+
+
+height = [1,8,6,2,5,4,8,3,7];
+console.log(maxArea(height));
