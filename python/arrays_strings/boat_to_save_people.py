61af154e18d77a0da62ca1d80f824bf0cc0d31ae
Basant Sharma <scriptclump@gmail.com>
2022-01-13 15:24:44 +0530

    Boats requires to save the peeple python solution

---
+++ python/arrays_strings/boat_to_save_people.py
@@ -0,0 +1,22 @@
+from typing import List
+
+class Solution:
+    def numRescueBoats(self, people: List[int], limit: int) -> int:
+        people.sort()
+        boats = 0
+        left = 0
+        right = len(people) -1
+        while ( left <= right):
+            if(people[left] + people[right] <= limit):
+                left+= 1
+                right-=1
+            else:
+                right-=1           
+            boats+=1
+        print(boats)
+        return boats
+
+s = Solution()
+people = [3,2,2,1]
+limit = 3
+s.numRescueBoats(people, limit)
