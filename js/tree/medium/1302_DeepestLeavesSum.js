35f333a21945a7de345defb964b5a4c34f7a2bc9
Basant Sharma <scriptclump@gmail.com>
2022-09-13 15:14:53 +0530

    Deepest leave sum BFS solution

--- js/tree/medium/1302_DeepestLeavesSum.js
+++ js/tree/medium/1302_DeepestLeavesSum.js
@@ -52,3 +52,22 @@ var deepestLeavesSum = function(root) {
     
 };
 
+//BFS Solution
+const deepestLeavesSum2 = function (root) {
+	let deepestLevel = 0,
+		sum = 0;
+	const queue = [root, null];
+	while (queue.length) {
+		const node = queue.shift();
+		if (node == null) {
+			if (!queue.length) return sum;
+			sum = 0;
+			deepestLevel++;
+			queue.push(null);
+			continue;
+		}
+		sum += node.val;
+		if (node.left) queue.push(node.left);
+		if (node.right) queue.push(node.right);
+	}
+};
