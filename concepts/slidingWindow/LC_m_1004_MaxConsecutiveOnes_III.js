d68809b6f99398a0a3cb198d2af9170de0362acc
Basant Sharma <scriptclump@gmail.com>
2022-11-19 22:37:43 +0530

    Adds sliding window solutions

--- concepts/slidingWindow/LC_m_1004_MaxConsecutiveOnes_III.js
+++ concepts/slidingWindow/LC_m_1004_MaxConsecutiveOnes_III.js
@@ -31,5 +31,16 @@
  * @return {number}
  */
  var longestOnes = function(nums, k) {
-    
+    let n = nums.length, max = 0, left = 0, map ={};
+    map[0] = 0; 
+    map[1] = 0;
+    for(let right = left; right < n; right++){
+        map[nums[right]]++;
+        while(right - left + 1 - map[1] > k){
+            map[nums[left]]--;
+            left++;
+        }
+        max = Math.max(max, right - left + 1);
+    }
+    return max;
 };
--- concepts/slidingWindow/LC_m_567_PermutationinString.js
+++ concepts/slidingWindow/LC_m_567_PermutationinString.js
@@ -35,5 +35,32 @@
  * @return {boolean}
  */
 var checkInclusion = function(s1, s2) {
-    
+    const s1Map = new Map()
+    for(const ch of s1) {
+        s1Map.set(ch, s1Map.get(ch)+1 || 1);
+    }
+    let left = 0, count =s1.length;
+    for(let right=0;right<s2.length;right++) {
+        const ch = s2[right]
+        if (s1Map.has(ch)) {
+            if (s1Map.get(ch) > 0) {
+                count--
+            }
+            s1Map.set(ch, s1Map.get(ch)-1)
+        }
+        if (right-left+1 <s1.length) {
+            continue
+        }
+        if (count == 0) {
+            return true
+        }
+        if (s1Map.has(s2[left])) {
+            if (s1Map.get(s2[left]) >= 0) {
+                count++
+            }
+            s1Map.set(s2[left], s1Map.get(s2[left])+1)
+        }
+        left++
+    }
+    return false
 };
