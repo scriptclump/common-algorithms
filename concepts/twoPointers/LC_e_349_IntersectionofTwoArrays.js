0bb5d2068c6da80c5d3b53b9408b24b67f5cf050
Basant Sharma <scriptclump@gmail.com>
2022-11-24 16:15:30 +0530

    Intersection between two arrays

--- concepts/twoPointers/LC_e_349_IntersectionofTwoArrays.js
+++ concepts/twoPointers/LC_e_349_IntersectionofTwoArrays.js
@@ -43,11 +43,19 @@
  * @param {*} nums2 
  */
 var intersectionBf = function(nums1, nums2) {
-    
+    let res = [];
+    for (let i = 0; i < nums1.length; i++) {
+        for (let j = 0; j < nums2.length; j++) {
+            if(nums1[i] === nums2[j] && res.includes(nums1[i]) === false ){
+                res.push(nums1[i]);
+            }
+        }
+    }
+    return res;
 };
 
 // Test Cases:
-const nums1 = [1,2,2,1], nums2 = [2,2]  // [2]
-// const nums1 = [4,9,5], nums2 = [9,4,9,8,4] //  [9,4] or [4,9]
+// const nums1 = [1,2,2,1], nums2 = [2,2]  // [2]
+//  const nums1 = [4,9,5], nums2 = [9,4,9,8,4] //  [9,4] or [4,9]
 
 console.log("Res", intersectionBf(nums1, nums2))
