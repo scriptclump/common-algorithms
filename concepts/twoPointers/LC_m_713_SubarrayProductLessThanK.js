558cdef1b2a4f881b13e84b4e24612715ac72f7c
Basant Sharma <scriptclump@gmail.com>
2022-12-08 17:08:18 +0530

    subarry product less than k

--- concepts/twoPointers/LC_m_713_SubarrayProductLessThanK.js
+++ concepts/twoPointers/LC_m_713_SubarrayProductLessThanK.js
@@ -35,6 +35,43 @@
  * @param {number} k
  * @return {number}
  */
- var numSubarrayProductLessThanK = function(nums, k) {
-    
+/*
+The key to this problem:
+
+Subarray size (right - left + 1) gives the number of new sub-arrays generated upon adding a new element to the subarray.
+
+For example, 
+What is the total number of sub-arrays of [10,4,2,1]?
+
+right: []
+add 10: [10] -> +1 -> [10]
+add 4: [10,4] -> +2 -> [4], [10,4]
+add 2: [10,4,2] -> +3 -> [2], [4,2], [10,4,2]
+add 1: [10,4,2,1] -> +4 -> [1], [2,1], [4,2,1], [10,4,2,1]
+
+Answer: 10
+
+To solve this question, we simply add an extra condition:
+is the subarray product less than K?
+*/
+var numSubarrayProductLessThanK = function(nums, k) {
+    if (nums === null || nums.length === 0) {
+        return 0;
+    }
+    if (k <= 1) return 0;
+    let right = 0, product = 1, count = 0;
+    for (let left = 0; left < nums.length; left++) {
+        product *= nums[left];
+        // ensure subarray product is less than k
+        while (product >= k && right <= left) {
+            product /= nums[right++];
+        }
+        count += left - right + 1;
+    }
+    return count;
+    // T.C: O(N)
+    // S.C: O(1)
 };
+const nums = [10,5,2,6], k = 100; // 8
+// const nums = [1,2,3], k = 0; // 0
+console.log("Res", numSubarrayProductLessThanK(nums, k));
